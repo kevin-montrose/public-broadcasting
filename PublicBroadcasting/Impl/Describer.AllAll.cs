2c4b24e7639c1eed4c2e10012afb58fc954486ac
Kevin Montrose <kevin.montrose@stackoverflow.com>
2012-11-19 20:43:04 -0500

    Another batch of describers

---
+++ PublicBroadcasting/Impl/Describer.AllAll.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicBroadcasting.Impl
+{
+    internal class AllAllDescriber<T>
+    {
+        private static readonly PromisedTypeDescription AllAllPromise;
+        private static readonly TypeDescription AllAll;
+
+        static AllAllDescriber()
+        {
+            var promiseType = typeof(PromisedTypeDescription<,>).MakeGenericType(typeof(T), typeof(AllAllDescriber<>).MakeGenericType(typeof(T)));
+            var promiseSingle = promiseType.GetField("Singleton");
+
+            AllAllPromise = (PromisedTypeDescription)promiseSingle.GetValue(null);
+
+            var res = Describer.BuildDescription(typeof(AllAllDescriber<>).MakeGenericType(typeof(T)));
+
+            AllAllPromise.Fulfil(res);
+
+            AllAll = res;
+        }
+
+        public static IncludedMembers GetMemberMask()
+        {
+            return IncludedMembers.Fields | IncludedMembers.Properties;
+        }
+
+        public static IncludedVisibility GetVisibilityMask()
+        {
+            return IncludedVisibility.Public | IncludedVisibility.Protected | IncludedVisibility.Internal | IncludedVisibility.Private;
+        }
+
+        public static TypeDescription Get()
+        {
+            // How does this happen you're thinking?
+            //   What happens if you call Get() from the static initializer?
+            //   That's how.
+            return AllAll ?? AllAllPromise;
+        }
+
+        public static TypeDescription GetForUse(bool flatten)
+        {
+            var ret = Get();
+
+            Action postPromise;
+            ret = ret.DePromise(out postPromise);
+            postPromise();
+
+            ret.Seal();
+
+            if (flatten)
+            {
+                ret = ret.Clone(new Dictionary<TypeDescription, TypeDescription>());
+
+                Flattener.Flatten(ret, Describer.GetIdProvider());
+            }
+
+            return ret;
+        }
+    }
+}
