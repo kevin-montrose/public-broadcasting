74787298addad3176c44c66011f8ca36ceddc025
Kevin Montrose <kevin.montrose@stackoverflow.com>
2012-12-02 23:19:31 -0500

    You always regret not having a version #

--- PublicBroadcasting/Impl/Envelope.cs
+++ PublicBroadcasting/Impl/Envelope.cs
@@ -11,15 +11,20 @@ namespace PublicBroadcasting.Impl
     [ProtoContract]
     internal class Envelope
     {
+        private const int CurrentVersion = 0;
+
         [ProtoMember(1)]
-        internal TypeDescription Description { get; set; }
+        internal byte Version { get; set; }
         [ProtoMember(2)]
+        internal TypeDescription Description { get; set; }
+        [ProtoMember(3)]
         internal byte[] Payload { get; set; }
 
         private Envelope() { }
 
         private Envelope(TypeDescription desc, byte[] payload)
         {
+            Version = CurrentVersion;
             Description = desc;
             Payload = payload;
         }
