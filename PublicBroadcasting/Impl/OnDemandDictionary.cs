feea0ba67ec4c116a54d793b59a860df182caac5
Kevin Montrose <kevin.montrose@stackoverflow.com>
2012-12-02 22:31:49 -0500

    Breaking some painfully large classes out into their own files

---
+++ PublicBroadcasting/Impl/OnDemandDictionary.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicBroadcasting.Impl
+{
+    internal class OnDemandDictionary<FromKey, FromVal, ToKey, ToVal> : IDictionary<ToKey, ToVal>
+    {
+        class Enumerator : IEnumerator<KeyValuePair<ToKey, ToVal>>, IEnumerator
+        {
+            public KeyValuePair<ToKey, ToVal> Current { get; private set; }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+
+            private IDictionaryEnumerator InnerEnumerable;
+            private OnDemandDictionary<FromKey, FromVal, ToKey, ToVal> Dictionary;
+            public Enumerator(OnDemandDictionary<FromKey, FromVal, ToKey, ToVal> dict)
+            {
+                Dictionary = dict;
+            }
+
+            public bool MoveNext()
+            {
+                if (InnerEnumerable == null)
+                {
+                    InnerEnumerable = Dictionary.InnerDictionary.GetEnumerator();
+                }
+
+                if (!InnerEnumerable.MoveNext())
+                {
+                    Current = default(KeyValuePair<ToKey, ToVal>);
+                    return false;
+                }
+
+                var entry = InnerEnumerable.Entry;
+
+                var key = entry.Key;
+                var val = entry.Value;
+
+                var mappedKey = (ToKey)Dictionary.KeyMapper(key);
+                var mappedVal = (ToVal)Dictionary.ValueMapper(val);
+
+                Current = new KeyValuePair<ToKey, ToVal>(mappedKey, mappedVal);
+
+                return true;
+            }
+
+            public void Reset()
+            {
+                InnerEnumerable = null;
+                Current = default(KeyValuePair<ToKey, ToVal>);
+            }
+
+            public void Dispose() { }
+        }
+
+        public ICollection<ToKey> Keys
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public ICollection<ToVal> Values
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int Count
+        {
+            get { return InnerDictionary.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return true; }
+        }
+
+        public ToVal this[ToKey key]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private IDictionary InnerDictionary;
+        private Func<object, object> KeyMapper;
+        private Func<object, object> ValueMapper;
+
+        public OnDemandDictionary(IDictionary innerDict, Func<object, object> keyMapper, Func<object, object> valMapper)
+        {
+            InnerDictionary = innerDict;
+            KeyMapper = keyMapper;
+            ValueMapper = valMapper;
+        }
+
+        public IEnumerator<KeyValuePair<ToKey, ToVal>> GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        #region Not Implemented
+
+        public void Add(ToKey key, ToVal value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(ToKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(ToKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(ToKey key, out ToVal value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(KeyValuePair<ToKey, ToVal> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<ToKey, ToVal> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<ToKey, ToVal>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<ToKey, ToVal> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ PublicBroadcasting/Impl/OnDemandList.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicBroadcasting.Impl
+{
+    internal class OnDemandList<From, To> : IList<To>
+    {
+        class Enumerator<A, B> : IEnumerator, IEnumerator<B>
+        {
+            object IEnumerator.Current
+            {
+                get { return this.Current; }
+            }
+
+            public B Current { get; private set; }
+
+            private int Index = -1;
+            private OnDemandList<A, B> List;
+
+            public Enumerator(OnDemandList<A, B> list)
+            {
+                List = list;
+            }
+
+            public bool MoveNext()
+            {
+                Index++;
+
+                if (Index >= List.Count) return false;
+
+                Current = List[Index];
+
+                return true;
+            }
+
+            public void Reset()
+            {
+                Index = -1;
+                Current = default(B);
+            }
+
+            public void Dispose() { }
+        }
+
+        public Type FromType { get { return typeof(From); } }
+        public Type ToType { get { return typeof(To); } }
+
+        public int Count
+        {
+            get { return UnderlyingList.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return true; }
+        }
+
+        public To this[int index]
+        {
+            get
+            {
+                var raw = UnderlyingList[index];
+                var mapped = Mapper(raw);
+
+                return (To)mapped;
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private IList UnderlyingList;
+        private Func<object, object> Mapper;
+
+        public OnDemandList(IList underlyingList, Func<object, object> mapper)
+        {
+            UnderlyingList = underlyingList;
+            Mapper = mapper;
+        }
+
+        public IEnumerator<To> GetEnumerator()
+        {
+            return new Enumerator<From, To>(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new Enumerator<From, To>(this);
+        }
+
+        #region Not Implemented
+
+        public int IndexOf(To item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, To item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(To item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(To item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(To[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(To item)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
